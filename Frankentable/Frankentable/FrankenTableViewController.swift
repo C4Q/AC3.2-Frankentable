47bc869b83e981f4a04b62651650fb24d816c862
Eric Chang <echang25@hotmail.com>
2016-11-27 18:12:43 -0500

    homework done

--- Frankentable/Frankentable/FrankenTableViewController.swift
+++ Frankentable/Frankentable/FrankenTableViewController.swift
@@ -9,89 +9,62 @@
 import UIKit
 
 class FrankenTableViewController: UITableViewController {
-
+    
+    var identifier = "WordCell"
+    var wordDict = [String:Int]()
+    var countDict = [String:Int]()
+    var abc = [Character]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         if let url = Bundle.main.url(forResource: "Data", withExtension: "txt"),
             let data = try? Data(contentsOf: url),
             let text = String(data: data, encoding: .utf8) {
-            
-            // here's your text
-            print(text)
+            setupABC()
+            organizeWords(text: text)
+            dump(wordDict.filter{ $0.key.characters.first == "a" })
         }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func setupABC() {
+        for char in "abcdefghijklmnopqrstuvwxyz".characters {
+            abc.append(char)
+        }
     }
-
+    
+    func organizeWords(text: String) {
+        var punctuation = CharacterSet()
+        punctuation.insert(charactersIn: ".,?!:;&()*+-@#$%^'~_<>` \n")
+        let wordArr = text.lowercased().components(separatedBy: punctuation).filter { !$0.isEmpty }
+        
+        for word in wordArr {
+            wordDict[word] = (wordDict[word] ?? 0) + 1
+        }
+    }
+    
     // MARK: - Table view data source
-
+    // MARK: - Table view data source
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 26
     }
-
+    
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return String(abc[section])
+    }
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        return wordDict.filter({ $0.key.characters.first == abc[section] }).count
     }
-
-    /*
+    
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
+        let cell = tableView.dequeueReusableCell(withIdentifier: identifier, for: indexPath) as! FrankenTableViewCell
+        let words = wordDict.filter({ $0.key.characters.first == abc[indexPath.section] }).sorted { $0.value > $1.value }
+        cell.wordTextLabel.text = "\(words[indexPath.row].key) - \(words[indexPath.row].value)"
+        
         return cell
     }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    
+    
 }
