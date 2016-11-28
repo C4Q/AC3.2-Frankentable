b8c833ebdd52df70697c5843cba176cab18c182c
Tom Seymour <tomeseymour44@gmail.com>
2016-11-28 17:49:00 -0500

    A-Z sections

--- Frankentable/Frankentable/FrankenTableViewController.swift
+++ Frankentable/Frankentable/FrankenTableViewController.swift
@@ -9,6 +9,8 @@
 import UIKit
 
 class FrankenTableViewController: UITableViewController {
+    
+    var words: [Word] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,79 +21,51 @@ class FrankenTableViewController: UITableViewController {
             
             // here's your text
             print(text)
+            self.words = self.wordsWithCount(str: text)
         }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func wordsWithCount(str: String) -> [Word] {
+        var wordDict = [String: Int]()
+        let justTheWordsArr = str.components(separatedBy: CharacterSet.punctuationCharacters.union(.whitespacesAndNewlines)).filter { !$0.isEmpty }
+        
+        for word in justTheWordsArr {
+            let lcWord = word.lowercased()
+            wordDict[lcWord] = (wordDict[lcWord] ?? 0) + 1
+        }
+        let sortedWordArr = wordDict.sorted { $0.value > $1.value }
+        
+        return sortedWordArr.map { Word.init(name: $0.key.capitalized, count: $0.value) }
     }
+
 
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 26
+    }
+    
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return String(Character(UnicodeScalar(65 + section)!))
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        let letter = String(Character(UnicodeScalar(65 + section)!))
+        print(".......... \(letter)")
+        let sectionPredicate = NSPredicate(format: "name BEGINSWITH '\(letter)'")
+        
+        return words.filter { sectionPredicate.evaluate(with: $0) }.count
     }
 
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
+        let letter = String(Character(UnicodeScalar(65 + indexPath.section)!))
+        let sectionPredicate = NSPredicate(format: "name BEGINSWITH '\(letter)'")
+        let thisWord = words.filter { sectionPredicate.evaluate(with: $0) }[indexPath.row]
+        cell.textLabel?.text = "\(thisWord.name): (\(thisWord.count))"
 
         return cell
     }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    
 }
