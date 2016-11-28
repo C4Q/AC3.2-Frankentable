b342a12bc14d4327512ee472fad1b72dfdaeb146
Victor Zhong <viczhong@gmail.com>
2016-11-28 01:25:23 -0500

    Basic homework done

--- Frankentable/Frankentable/FrankenTableViewController.swift
+++ Frankentable/Frankentable/FrankenTableViewController.swift
@@ -18,25 +18,49 @@ class FrankenTableViewController: UITableViewController {
     @IBAction func switchSwitched(_ sender: UISwitch) {
         wordSorted = !wordSorted
         self.tableView.reloadData()
+        self.tableView.setContentOffset(CGPoint.zero, animated: false)
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         if let url = Bundle.main.url(forResource: "Data", withExtension: "txt"),
             let data = try? Data(contentsOf: url),
             let text = String(data: data, encoding: .utf8) {
             
-            // here's your text
-//            print(text)
             wordCount = mostCommonWords(string: text)
             print(wordCount)
             letterSection = firstLetters(dict: wordCount)
             valueSection = countValues(dict: wordCount)
         }
         
+        //        getData(endPoint: "http://www.gutenberg.org/cache/epub/84/pg84.txt") {  (data) in
+        //            if data != nil {
+        //                if let text = String(data: data!, encoding: .utf8) {
+        //                    self.wordCount = self.mostCommonWords(string: text)
+        //                    print(self.wordCount)
+        //                    self.letterSection = self.firstLetters(dict: self.wordCount)
+        //                    self.valueSection = self.countValues(dict: self.wordCount)
+        //                    DispatchQueue.main.async {
+        //                        self.tableView.reloadData()
+        //                    }
+        //                }
+        //            }
+        //        }
     }
-
+    
+    //    func getData(endPoint: String, callback: @escaping (Data?) -> Void) {
+    //        guard let myURL = URL(string: endPoint) else { return }
+    //        let session = URLSession(configuration: URLSessionConfiguration.default)
+    //        session.dataTask(with: myURL) { (data: Data?, response: URLResponse?, error: Error?) in
+    //            if error != nil {
+    //                print("Error during session: \(error)")
+    //            }
+    //            guard let validData = data else { return }
+    //            callback(validData)
+    //            }.resume()
+    //    }
+    
     func mostCommonWords (string: String) -> [String : Int] {
         var wordDict = [String: Int]()
         let wordArr = string.lowercased().components(separatedBy: CharacterSet.punctuationCharacters.union(CharacterSet.whitespaces).union(CharacterSet.newlines)).filter { !$0.isEmpty }
@@ -47,12 +71,15 @@ class FrankenTableViewController: UITableViewController {
         
         return wordDict
     }
-
+    
     func firstLetters(dict: [String : Int]) -> [String] {
         var tempDict = [String : String]()
         
         for entry in dict {
-            tempDict[String(entry.key.characters.prefix(1))] = "Sup"
+            let first = String(entry.key.characters.prefix(1))
+            if Int(first) == nil {
+                tempDict[first] = "Sup"
+            }
         }
         
         return Array(tempDict.keys).sorted()
@@ -65,11 +92,21 @@ class FrankenTableViewController: UITableViewController {
             tempDict[entry.value] = 1
         }
         
-        return Array(tempDict.keys).sorted {$0 > $1}
+        return Array(tempDict.keys).sorted { $0 > $1 }
+    }
+    
+    // sort words for use in rows
+    func sortedWord(at indexPath: IndexPath) -> (key: String, value: Int) {
+        if wordSorted {
+            return wordCount.filter { String($0.key.characters.prefix(1)) == letterSection[indexPath.section] }.sorted { $0.key < $1.key } [indexPath.row]
+        }
+        else {
+            return wordCount.filter { $0.value == valueSection[indexPath.section] }.sorted { $0.value > $1.value } [indexPath.row]
+        }
     }
     
     // MARK: - Table view data source
-
+    
     override func numberOfSections(in tableView: UITableView) -> Int {
         if wordSorted {
             return letterSection.count
@@ -81,37 +118,28 @@ class FrankenTableViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
         if wordSorted {
-            return letterSection[section]
+            return letterSection[section].uppercased()
         }
         else {
             return String(valueSection[section])
         }
     }
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         if wordSorted {
-            return wordCount.filter { String($0.key.characters.prefix(1)) == letterSection[section]}.count
+            return wordCount.filter { String($0.key.characters.prefix(1)) == letterSection[section] }.count
         }
         else {
-            return wordCount.filter {$0.value == valueSection[section]}.count
+            return wordCount.filter { $0.value == valueSection[section] }.count
         }
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "wordCellReuse", for: indexPath)
-//        var word = [String : Int]()
-        if wordSorted {
-            let word = wordCount.filter { String($0.key.characters.prefix(1)) == letterSection[indexPath.section]}.sorted {$0.key < $1.key}[indexPath.row]
-            cell.textLabel?.text = "\(word.key) (\(word.value))"
-            return cell
-        }
-        else {
-            let word = wordCount.filter {$0.value == valueSection[indexPath.section]}.sorted {$0.value > $1.value}[indexPath.row]
-            cell.textLabel?.text = "\(word.key) (\(word.value))"
-            return cell
-        }
-
+        let word = sortedWord(at: indexPath)
         
+        cell.textLabel?.text = "\(word.key) (\(word.value))"
+        return cell
     }
-
+    
 }
